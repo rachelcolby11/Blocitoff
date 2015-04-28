9ec43dee5853ec0a0dea0f0b50616879555975f9
Rachel Colby <rachelcolby11@gmail.com>
2015-04-28 13:10:33 -0700

    Update user and item routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users 
+  resources :users, only: [:show] do
+    resources :items, only: [:create]
+  end
+
   get 'welcome/index'
 
   get 'welcome/about'
